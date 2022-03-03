a8c92a58a488755f4f76a1569f417ab8170ac76c
develper613 <87280497+develper613@users.noreply.github.com>
2022-03-02 20:04:29 -0500

    Added header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import React from "react";
 import {Home} from '../home/Home';
-import {Todo} from '../todo/Todo';
 import TodoList from '../todo_youTube/TodoList';
+import {Header} from '../header/header';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 
 function App() {
-
   return (
-    
     <div>
-      <TodoList />
+     
+  <BrowserRouter>
+  <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/todo" element={<TodoList />} />
+    </Routes>
+  </BrowserRouter>
+
     </div>
   );
 }
